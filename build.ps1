4e29163dd5da00a25ab2ce9fbcc47681e177035d
Gavreg <faktorial5@gmail.com>
2024-12-25 01:49:39 +0300

    cleanup L2 and it's images

---
+++ build.ps1
@@ -0,0 +1,18 @@
+
+$ImagesFolders = @(
+"Images\L2"
+)
+
+foreach($folder in $ImagesFolders)
+{
+    Write-Host ("Cобираю файлы в $folder...");
+
+    $texFiles = Get-ChildItem -Path $folder -Filter "*.tex" -Recurse
+
+    foreach ($file in $texFiles) {
+        Write-Host "   Компиляция файла: '$($file.FullName)'"
+        
+        & xelatex -synctex=1 -interaction=nonstopmode --shell-escape -8bit -output-directory="$($file.DirectoryName)" "$($file.FullName)"
+
+    }
+}
