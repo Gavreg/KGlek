8f54c668ce666c73bb5bc49df8cb4e663fab3bab
Gavreg <faktorial5@gmail.com>
2024-12-26 12:26:32 +0300

    adding L5

--- build.ps1
+++ build.ps1
@@ -4,9 +4,10 @@
 $PSVersionTable.PSVersion
 
 $ImagesFolders = @(
-    "Images\L2"
+    #"Images\L2"
     "Images\L3"
-    "Images\L4"
+    #"Images\L4"
+    #"Images\L5"
 )
 
 $texfiles = @();
@@ -24,7 +25,9 @@ Write-Host ""
 Write-Host ("Cобираю файлы:");
 Write-Host ""
 
-$texFiles | ForEach-Object -Parallel { 
+$jobs = ($texFiles | ForEach-Object -Parallel { 
+
+    cd "$($_.DirectoryName)" 
     $null = xelatex -synctex=1 -interaction=nonstopmode -halt-on-error --shell-escape -8bit -output-directory="$($_.DirectoryName)" "$($_.FullName)" 
     if ($LastExitCode -ne 0) {
         Write-Host "    !!Ошибка: '$($_.FullName)'. См. лог сборки" -ForegroundColor red
@@ -32,7 +35,19 @@ $texFiles | ForEach-Object -Parallel {
     else {
         Write-Host "    Успешно!: '$($_.FullName)'." -ForegroundColor green
     }
-} -AsJob  |  Wait-Job | Receive-Job
+} -AsJob)  
+
+$jobs = $jobs.ChildJobs 
+
+$total = $jobs.Count
+
+While($jobs.Count -ne 0)
+{
+    $job = ($jobs | Wait-Job -Any)
+    Write-Host "$($jobs.Count)/$total " -NoNewline
+    $job | Receive-Job
+    $jobs = ($jobs | Where-Object{$_-ne $job})
+}
 
 
 Write-Host ""
