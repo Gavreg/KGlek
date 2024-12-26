69d3f5f42e7dff6577945e5e7f2205ca00999eb5
Gavreg <faktorial5@gmail.com>
2024-12-26 11:18:30 +0300

    refactoring L4 and it's images

--- build.ps1
+++ build.ps1
@@ -1,27 +1,40 @@
 
 #Сборка изображений
 
+$PSVersionTable.PSVersion
+
 $ImagesFolders = @(
-#"Images\L2",
-"Images\L3"
+    "Images\L2"
+    "Images\L3"
+    "Images\L4"
 )
 
-foreach($folder in $ImagesFolders)
-{
-    Write-Host ("Cобираю файлы в $folder...");
-
-    $texFiles = Get-ChildItem -Path $folder -Filter "*.tex" -Recurse
-
-    foreach ($file in $texFiles) {
-        Write-Host "   Компиляция файла: '$($file.FullName)'"
-        
-        $null = xelatex -synctex=1 -interaction=nonstopmode --shell-escape -8bit -output-directory="$($file.DirectoryName)" "$($file.FullName)" 
-
-        if ($LastExitCode -ne 0)
-        {
-            Write-Host "      !!Ошибка: '$($file.FullName)'. См. лог сборки" -ForegroundColor red
-        }
-
-    }
+$texfiles = @();
+foreach ($folder in $ImagesFolders) {
+    $texFiles += Get-ChildItem -Path $folder -Filter "*.tex" -Recurse
 }
 
+
+Write-Host ""
+Write-Host ("Файлы на сбор:");
+Write-Host ""
+$texFiles | ForEach-Object {$_.FullName}
+
+Write-Host ""
+Write-Host ("Cобираю файлы:");
+Write-Host ""
+
+$texFiles | ForEach-Object -Parallel { 
+    $null = xelatex -synctex=1 -interaction=nonstopmode -halt-on-error --shell-escape -8bit -output-directory="$($_.DirectoryName)" "$($_.FullName)" 
+    if ($LastExitCode -ne 0) {
+        Write-Host "    !!Ошибка: '$($_.FullName)'. См. лог сборки" -ForegroundColor red
+    }
+    else {
+        Write-Host "    Успешно!: '$($_.FullName)'." -ForegroundColor green
+    }
+} -AsJob  |  Wait-Job | Receive-Job
+
+
+Write-Host ""
+Write-Host ("Завершено!");
+Write-Host ""
