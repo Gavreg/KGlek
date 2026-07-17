e96072e72359774d423d01923951b9214462e28d
Gavreg <faktorial5@gmail.com>
2026-07-18 00:34:34 +0300

    проверка кеша

--- build.sh
+++ build.sh
@@ -13,15 +13,15 @@ sudo docker build -t "$teximage" .
 for d in ./Images/CG_*
 do
     [ -d "$d" ] || continue
-    echo "====================="
+    echo "!======================================!"
     echo "Building images in $d"
-    echo "====================="
+    echo "!======================================!"
     for f in "$d"/*.tex
     do
         [ -f "$f" ] || continue
-        echo "================="
+        echo "!==================================!"
         echo "Building file  $f"
-        echo "================="
+        echo "!==================================!"
         sudo docker run --rm -v "$PWD":"$workdir" -w "$workdir" -e f="$f" -e d="$d" -e latexcmd="$latexcmd" "$teximage" sh -c 'cd "$d" && echo "compiling $f" && eval "$latexcmd \"\$(basename \"\$f\")\""'
 
         echo ""
@@ -32,7 +32,10 @@ done
 for f in CG_*.tex
 do
     [ -f "$f" ] || continue
-     sudo docker run --rm -v "$PWD":"$workdir" -w "$workdir" "$teximage" $latexcmd "$f"
+    echo "!==================================!"
+    echo "Building file  $f"
+    echo "!==================================!"
+    sudo docker run --rm -v "$PWD":"$workdir" -w "$workdir" "$teximage" $latexcmd "$f"
 done
 
 
