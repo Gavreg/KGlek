48e6089021f09f315f3f33c39389e8461e9d48d1
Gavreg <faktorial5@gmail.com>
2026-07-21 17:03:23 +0300

    Оптимизация workwlow

--- build.sh
+++ build.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
 set -e
 
-export teximage="teximage"
-export workdir=/data
-export fonts=/usr/share/fonts
-
 export latexcmd="latexmk  -pdfxe -interaction=nonstopmode -halt-on-error -synctex=1 -8bit --shell-escape"
 
-sudo docker build -t "$teximage" .
+workdir=$(pwd)
 
-
-for d in ./Images/CG_*
+for f in ./Images/CG_*/*.tex
 do
-    [ -d "$d" ] || continue
-    echo "!======================================!"
-    echo "Building images in $d"
-    echo "!======================================!"
-    for f in "$d"/*.tex
-    do
-        [ -f "$f" ] || continue
-        echo "!==================================!"
-        echo "Building file  $f"
-        echo "!==================================!"
-        sudo docker run --rm -v "$PWD":"$workdir" -w "$workdir" -e f="$f" -e d="$d" -e latexcmd="$latexcmd" "$teximage" sh -c 'cd "$d" && echo "compiling $f" && eval "$latexcmd \"\$(basename \"\$f\")\""'
-
-        echo ""
-    done
+    [ -f "$f" ] || continue
+    
+    echo "!==================================!"
+    echo "Building file  $f"
+    echo "!==================================!"
+    
+    texfile=$(basename "$f")
+    texfiledir=&(dirname "$f")
+    
+    cd "$texfiledir"
+    
+    echo "$latexcmd \"\$(basename \"\$f\")\""
+    
+    echo ""
+    
+    cd "$workdir"
 done
 
 
@@ -35,8 +32,6 @@ do
     echo "!==================================!"
     echo "Building file  $f"
     echo "!==================================!"
-    sudo docker run --rm -v "$PWD":"$workdir" -w "$workdir" "$teximage" $latexcmd "$f"
+    
+    echo $latexcmd "$f"
 done
-
-
-sudo docker image rm "$teximage"
